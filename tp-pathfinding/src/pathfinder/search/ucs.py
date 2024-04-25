3ae260de196764978d842bb7e73e31e97b4de34c
mirandaonega <mirandaonega@gmail.com>
2024-04-25 19:13:59 -0300

    add: ucs

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -19,7 +19,36 @@ class UniformCostSearch:
         node = Node("", grid.start, 0)
 
         # Initialize the explored dictionary to be empty
-        explored = {} 
+        explored = {node.state: node} 
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, node.cost)
+
+        while True:
+            #
+            if frontier.is_empty():
+                return NoSolution(explored)
+            
+            node = frontier.pop()
+
+            if node.state==grid.end:
+                return Solution(node, explored)
+            
+            successors = grid.get_neighbours(node.state)
+
+            for action, position in successors.items():
+                c_cost = node.cost + grid.get_cost(position)
+
+                if position not in explored or c_cost < explored[position].cost:
+                    c_node = Node(
+                        value="",
+                        state = position, 
+                        cost = c_cost,
+                        parent = node, 
+                        action = action
+                    )
+
+                frontier.add(c_node, c_node.cost)
+                explored[c_node.state] = c_node
         
         # Add the node to the explored dictionary
         explored[node.state] = True
