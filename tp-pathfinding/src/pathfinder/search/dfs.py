aec8dadd643be9037b26fbb9f23f1dbd5ab895ce
mirandaonega <mirandaonega@gmail.com>
2024-04-25 19:44:03 -0300

    agrega dfs

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -17,11 +17,46 @@ class DepthFirstSearch:
         """
         # Initialize a node with the initial position
         node = Node("", grid.start, 0)
+        if node.state == grid.end:
+            return Solution(node) #revisar
+        frontier = StackFrontier()
+        frontier.add(node) 
+
 
         # Initialize the explored dictionary to be empty
-        explored = {} 
+        expanded = {} 
+        
+        while True:
+            if frontier.is_empty():
+                return NoSolution() #REVISAR
+            
+            node = frontier.pop()
+
+            if node.state in expanded:
+                continue
+        
+            expanded[node.state] = node
+
+            successors = grid.get_neighbours(node.state)
+
+            for action, position in successors.items():
+                p_solucion = node.state #revisar
+
+                if p_solucion not in expanded:
+                    p_node = Node(
+                        value=p_solucion,
+                        state = node, 
+                        cost = node.cost + grid.get_cost(position),
+                        parent = node, 
+                        action = action
+                    )
+
+                if p_solucion == grid.end:
+                    return Solution(p_node)
+                
+                frontier.add(p_node)
         
         # Add the node to the explored dictionary
-        explored[node.state] = True
+        expanded[node.state] = True
         
-        return NoSolution(explored)
+        return NoSolution(expanded)
