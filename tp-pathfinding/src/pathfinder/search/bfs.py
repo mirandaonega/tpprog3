6407d177d517a05fc85205af780a4654ea42826c
mirandaonega <mirandaonega@gmail.com>
2024-04-25 19:33:22 -0300

    agrega bfs

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -17,11 +17,41 @@ class BreadthFirstSearch:
         """
         # Initialize a node with the initial position
         node = Node("", grid.start, 0)
+        if node.state == grid.end:
+            return Solution(node) #revisar
 
         # Initialize the explored dictionary to be empty
-        explored = {} 
+        explored = {node.state: node}
+        frontier = QueueFrontier()
+        frontier.add(node) 
         
-        # Add the node to the explored dictionary
+        while True:
+            if frontier.is_empty():
+                return NoSolution() #REVISAR
+            
+            node = frontier.pop()
+
+            successors = grid.get_neighbours(node.state)
+
+            for action, position in successors.items():
+                p_solucion = node.state #revisar
+                if p_solucion not in explored:
+                    p_node = Node(
+                        value=p_solucion,
+                        state = node, 
+                        cost = node.cost + grid.get_cost(position),
+                        parent = node, 
+                        action = action
+                    )
+
+                    if p_solucion == grid.end:
+                        return Solution(p_node)
+                    
+                    explored[p_solucion.state] = p_node #revisar
+
+                    frontier.add(p_node)
+
+"""        # Add the node to the explored dictionary
         explored[node.state] = True
         
-        return NoSolution(explored)
+        return NoSolution(explored)"""
